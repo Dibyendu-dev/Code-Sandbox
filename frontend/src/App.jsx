bdc418c66e5bf7437d6ee6a7e571b83882592590
ddas4548 <ddas4548@gmail.com>
2025-10-15 21:39:19 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import { CreateProject } from './pages/CreateProject'
+
+function App() {
+  
+
+  return (
+    <>
+      <Routes>
+      <Route path="/" element={<CreateProject />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
